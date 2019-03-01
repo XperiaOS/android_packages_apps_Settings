PixelExperience/packages_apps_Settings

Implement button settings
Credits to Steve Kondik and CM/LineageOS

https://github.com/LineageOS/android_packages_apps_LineageParts/commit/3281bd65933aa09c410cf1258040ed7d0c0fc1b3#diff-c8fb7086712ea95726e6465e32fceb54

Change-Id: I9a271e0980767d4a7e61d9d5366a558722f59ea8

Android.mk
@@ -37,7 +37,8 @@ LOCAL_JAVA_LIBRARIES := \
     bouncycastle \
     telephony-common \
     ims-common \
-    telephony-ext
+    telephony-ext \
+    org.pixelexperience.keydisabler
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-arch-lifecycle-runtime \

src/com/android/settings/custom/buttons/ButtonSettingsUtils.java
@@ -1,0 +1,29 @@
+/*
+* Copyright (C) 2018 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.custom.buttons;
+
+import android.content.Context;
+import org.pixelexperience.keydisabler.KeyDisabler;
+
+public class ButtonSettingsUtils {
+    public static boolean isAvailable(Context context) {
+        return (KeyDisabler.isSupported() || isHWKeysAvailable(context));
+    }
+    public static boolean isHWKeysAvailable(Context context){
+        int deviceKeys = context.getResources().getInteger(com.android.internal.R.integer.config_deviceHardwareKeys);
+        return deviceKeys != 64;
+    }
+}

XperiaOS/android_packages_apps_Settings

Android.mk
@@ -37,7 +37,8 @@ LOCAL_JAVA_LIBRARIES := \
     bouncycastle \
     telephony-common \
     ims-common \
-    telephony-ext
+    telephony-ext \
+    org.xperiaos.keydisabler
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-arch-lifecycle-runtime \

src/com/android/settings/custom/buttons/ButtonSettingsUtils.java
@@ -1,0 +1,29 @@
+/*
+* Copyright (C) 2018 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.custom.buttons;
+
+import android.content.Context;
+import org.xperiaos.keydisabler.KeyDisabler;
+
+public class ButtonSettingsUtils {
+    public static boolean isAvailable(Context context) {
+        return (KeyDisabler.isSupported() || isHWKeysAvailable(context));
+    }
+    public static boolean isHWKeysAvailable(Context context){
+        int deviceKeys = context.getResources().getInteger(com.android.internal.R.integer.config_deviceHardwareKeys);
+        return deviceKeys != 64;
+    }
+}